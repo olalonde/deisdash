464f90f000e3b3b808e4a641683cb3b579b008c6
Felipe Cavalcanti <felipe.cavalcanti@tfgco.com>
2016-08-31 18:57:57 -0300

    add UserAdd form

--- containers/Dash/Users/index.js
+++ containers/Dash/Users/index.js
@@ -5,6 +5,59 @@ import { connect } from 'react-redux'
 import UserItem from './UserItem'
 import { Table } from 'react-bootstrap'
 
+class CreateUser extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.create = this.create.bind(this)
+    this.change = this.change.bind(this)
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+    }
+  }
+
+  change(type, field) {
+    const nextState = {}
+    nextState[type] = field.target.value
+    this.setState(nextState)
+  }
+
+  create() {
+    this.props.createUser(this.state.username, this.state.password, this.state.email)
+    this.setState({ username: '', password: '', email: '' })
+  }
+
+  render() {
+    return (
+      <div className="form-inline create-user-form">
+        <div className="form-group">
+          <input
+            value={this.state.username}
+            onChange={this.change.bind(this, 'username')}
+            className="form-control"
+            placeholder="username"
+          />
+          <input type="password"
+            value={this.state.password}
+            onChange={this.change.bind(this, 'password')}
+            className="form-control"
+            placeholder="password"
+          />
+          <input
+            value={this.state.email}
+            onChange={this.change.bind(this, 'email')}
+            className="form-control"
+            placeholder="email"
+          />
+        </div>
+        <button className="btn btn-success" onClick={this.create}>Create user</button>
+      </div>
+    )
+  }
+}
+
 class Users extends React.Component {
   constructor(props) {
     super(props)
@@ -49,7 +102,7 @@ class Users extends React.Component {
     if (!users) return <div></div>
 
     // Sort by username
-    users.sort(function(a,b) {return (a.username > b.username) ? 1 : ((b.username > a.username) ? -1 : 0);} );
+    users.sort(function( a,b) {return (a.username > b.username) ? 1 : ((b.username > a.username) ? -1 : 0);} );
 
     const userItems = users.map((user) => (
       <UserItem
@@ -84,6 +137,7 @@ class Users extends React.Component {
             {userItems}
           </tbody>
         </Table>
+        <CreateUser createUser={this.createUser} />
       </div>
     )
   }
